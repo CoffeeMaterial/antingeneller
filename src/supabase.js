4d16608a39222ad98450575b311a8efc03e20ec8
Daniel <dm.mira@outlook.com>
2025-08-02 18:06:01 +0200

    Integrate Supabase id and key to load and save questions 3

--- src/supabase.js
+++ src/supabase.js
@@ -2,6 +2,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = "https://fpbgvbnldvxswldsdhtu.supabase.co";
-const supabaseKey = "sb_publishable_g5KxRYARF3VW2dMvr08uuw_BMdDdjJk";
+const supabaseKey = process.env.SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
